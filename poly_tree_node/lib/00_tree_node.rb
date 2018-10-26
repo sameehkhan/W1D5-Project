514cfbeafa5f162be47e158285fe94471e37d895
sameehkhan <sameehkhan1@gmail.com>
2018-10-26 14:46:32 -0700

    Add #parent= and #add_child

--- poly_tree_node/lib/00_tree_node.rb
+++ poly_tree_node/lib/00_tree_node.rb
@@ -1,3 +1,44 @@
-class PolyTreeNode
+require 'byebug'
 
+class PolyTreeNode
+  
+
+    def initialize(value)
+      @value = value 
+      @children = []
+      @parent = nil
+    end
+    
+    def parent=(val)
+      @parent.children.delete(self) if @parent
+
+      @parent = val
+      if val && @parent.children.none? { |el| el == self}
+          @parent.children << self
+      end 
+    end
+    
+    def parent 
+      parent
+    end
+    
+    def children 
+      children
+    end
+    
+    def value
+      value
+    end
+    
+    def add_child(child_node)
+      children << child_node
+      child_node.parent = self 
+    end 
+    
+    def remove_child(child_node)
+      
+    end
+    
+    attr_reader :parent, :children, :value
+    
 end
