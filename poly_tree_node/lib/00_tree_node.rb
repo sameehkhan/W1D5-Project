7e50d13e9016088979338166b9dd634483084e0f
sameehkhan <sameehkhan1@gmail.com>
2018-10-26 14:54:45 -0700

    Add #add_child and #remove_child

--- poly_tree_node/lib/00_tree_node.rb
+++ poly_tree_node/lib/00_tree_node.rb
@@ -32,11 +32,17 @@ class PolyTreeNode
     
     def add_child(child_node)
       children << child_node
+      
       child_node.parent = self 
     end 
     
     def remove_child(child_node)
-      
+      if children.include?(child_node)
+        children.delete(child_node)
+        child_node.parent = nil
+      else
+        raise error 
+      end 
     end
     
     attr_reader :parent, :children, :value
